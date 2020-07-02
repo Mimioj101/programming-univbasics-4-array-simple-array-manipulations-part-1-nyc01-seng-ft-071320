940635de5d071b65e426d2f638d720a2531a28a6
Mimi Ojserkis <mimioj@gmail.com>
2020-07-02 17:51:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,11 +7,18 @@ def using_unshift(arr,element)
   arr.unshift(element)
 end
 
+
 def using_pop(arr)
   arr.pop
 end
 
+
 def pop_with_args(arr)
   popped_elements = arr.pop(2)
   popped_elements
+end 
+
+
+def using_shift(arr)
+  arr.shift
 end 
